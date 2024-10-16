01f626d963f9d79ec0ff0c097bdecdf0f45f5143
kartoone76 <115804135+kartoone76@users.noreply.github.com>
2024-10-16 16:33:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function downloadData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
-  console.log(startdat);
+  console.log(startdate);
   console.log(enddate);
 }
